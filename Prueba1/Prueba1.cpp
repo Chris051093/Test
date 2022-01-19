3ecd37ad944b97d8893c0e9e72dc5e5fe9efd0d0
Alejandro Enriquez <aelejandro9@gmail.com>
2022-01-19 14:33:26 -0700

    cpp, header and main remaked

--- Prueba1/Prueba1.cpp
+++ Prueba1/Prueba1.cpp
@@ -2,10 +2,17 @@
 //
 
 #include <iostream>
-
+#include "math.h"
 using namespace std;
 
 int main()
 {
+    int x, y;
     cout << "Hello World!\n";
+    cout << "give me 2 integers: ";
+    cin >> x >> y;
+    cout << "sum: " << sum(x, y)<< endl;
+    cout << "substraction: " << sub(x, y) << endl;
+    cout << "mult: " << mult(x, y) << endl;
+    cout << "div: " << divi(x, y) << endl;
 }
---
+++ Prueba1/math.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b);
+int sub(int a, int b);
+int mult(int a, int b);
+int divi(int a, int b);
